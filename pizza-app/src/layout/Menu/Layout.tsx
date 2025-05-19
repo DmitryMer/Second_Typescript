aab6f50c52be158b564a59be1c45ef0d6da7a56c
Dmitry <duduff9317@gmail.com>
2025-05-19 10:44:02 +0300

    add store and slice

--- pizza-app/src/layout/Menu/Layout.tsx
+++ pizza-app/src/layout/Menu/Layout.tsx
@@ -1,4 +1,4 @@
-import { Link, NavLink, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
 import styles from "./Layout.module.css";
 import menu from "../../public/menu-icon.svg";
 import cart from "../../public/cart-icon.svg";
@@ -8,6 +8,11 @@ import Button from "../../components/Button/Button";
 import cn from "classnames";
 
 export function Layout() {
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.removeItem("jwt");
+    navigate("/auth/login");
+  };
   return (
     <div className={styles["layout"]}>
       <div className={styles["sidebar"]}>
@@ -44,7 +49,7 @@ export function Layout() {
             Корзина
           </NavLink>
         </div>
-        <Button className={styles["exit"]}>
+        <Button className={styles["exit"]} onClick={logout}>
           <img src={exit} alt="иконка выхода"></img>
           Выход
         </Button>
--- pizza-app/src/main.tsx
+++ pizza-app/src/main.tsx
@@ -13,6 +13,8 @@ import { AuthLayout } from "./layout/Auth/AuthLayout.tsx";
 import { Login } from "./pages/Login/Login.tsx";
 import { Register } from "./pages/Register/Register.tsx";
 import { RequireAuth } from "./helpers/RequireAuth.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 const Menu = lazy(() => import("./pages/Menu/Menu"));
 
@@ -72,6 +74,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
